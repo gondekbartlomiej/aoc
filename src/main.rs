5f44000e9f1a793f7d6273e4a24982c2b8c5df8f
Bartlomiej Gondek <gondekbartlomiej@gmail.com>
2023-12-03 15:20:43 +0100

    template main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,7 @@
+use std::fs;
+
 fn main() {
+    let contents = fs::read_to_string("input_files/day_x/partx.txt").expect("No such file found!");
+    let number:u32 = contents.lines().map(parse_line).sum();
+    println!("The result is: {number}");
 }
